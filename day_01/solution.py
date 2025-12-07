f49b0c836f433a71f568f580584dc17799e5aa4e
Rob Passmore <robert.passmore@generationhome.com>
2025-12-07 15:35:42 +0000

    --wip-- [skip ci]

--- day_01/solution.py
+++ day_01/solution.py
@@ -3,7 +3,15 @@ import os
 
 def process_input(data):
     """
-    Placeholder function to process the input data.
+    Calculate the number of times a combination dial of 99 spaces lands on 0
+    after following set instructions.
+
+    Instructions come in a series of left and right turns of the dial.
+    These are relative to the current position of the dial.
+    The dial starts at 50.
+    i.e. L33 moves the dial to 17. R8 moves the dial to 25. etc.
+    The dial wraps. i.e. L50 moves the dial to 0. L1 moves the dial to 99. etc.
+
 
     Args:
         data: The contents of the input file as a string
@@ -12,7 +20,29 @@ def process_input(data):
         The processed result
     """
     # TODO: Implement your solution here
-    return 3
+    instructions = data.split("\n")
+    position = 50
+    zero_count = 0
+    print("Input: ", instructions)
+    print("starting position: ", position)
+    for instruction in instructions:
+        direction = instruction[0] 
+        distance = int(instruction[1:])
+        if direction == 'L':
+            position -= int(instruction[1:])
+        else:
+            position += int(instruction[1:])
+
+        position = position % 99
+
+        if position < 0:
+            position += 100
+
+        if position == 0:
+            zero_count += 1
+        print("position: ", position)
+
+    return zero_count
 
 
 def main():
